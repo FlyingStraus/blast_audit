2703ac95cc6146a176896951ac4b06fb0b08cb10
FlyingStraus <105660597+FlyingStraus@users.noreply.github.com>
2024-02-02 22:23:56 +0600

    chages

--- blast-optimism/packages/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
+++ blast-optimism/packages/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSL 1.1 - Copyright 2024 MetaLayer Labs Ltd.
 pragma solidity 0.8.15;
-
+// @audit check - changes
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { SafeCall } from "src/libraries/SafeCall.sol";
 import { Hashing } from "src/libraries/Hashing.sol";
